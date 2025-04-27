144cc2f51f6fca0adfe81bd2a2f7fa461347a8ef
barelyarealdude <barelyarealdude@gmail.com>
2025-04-26 22:12:48 -0500

    update gtk_ex.c
    update app id to make it standards-compliant with flatpak (for some god awful reason)

--- gtk_ex.c
+++ gtk_ex.c
@@ -16,7 +16,7 @@ static void activate (GtkApplication * app, gpointer user_data) {
 int main(int argc, char **argv) {
 	GtkApplication *app;
 	int status;
-	app = gtk_application_new("com.github.username.gtk-ex", G_APPLICATION_DEFAULT_FLAGS);
+	app = gtk_application_new("io.github.barelyarealdude.gtk_ex", G_APPLICATION_DEFAULT_FLAGS);
 	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 	status = g_application_run(G_APPLICATION(app), argc, argv);
 	
