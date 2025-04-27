4069e47635e459411dbc6838767fa1a88dae0a89
barelyarealdude <barelyarealdude@gmail.com>
2025-04-26 22:05:24 -0500

    added 'gtk_ex.c'
    created this entire repo + file just so the gtk3 app id would make sense.

---
+++ gtk_ex.c
@@ -0,0 +1,24 @@
+// GTK starting usage??
+
+// compilation commands --- gcc `pkg-config --cflags gtk+-3.0` -o gtk_ex gtk_ex.c `pkg-config --libs gtk+-3.0`
+
+#include <gtk/gtk.h>
+
+static void activate (GtkApplication * app, gpointer user_data) {
+	GtkWidget * window;
+	
+	window = gtk_application_window_new(app);
+	gtk_window_set_title(GTK_WINDOW(window), "Title");
+	gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+	gtk_widget_show_all(window);
+}
+
+int main(int argc, char **argv) {
+	GtkApplication *app;
+	int status;
+	app = gtk_application_new("com.github.username.gtk-ex", G_APPLICATION_DEFAULT_FLAGS);
+	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+	status = g_application_run(G_APPLICATION(app), argc, argv);
+	
+	return status;
+}
